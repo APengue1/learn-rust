5c2ae02b3f2797e45e01dd5f81036135ddfd05d5
Angelo Pengue <apengue@vigilantglobal.com>
2018-07-10 21:36:35 -0400

    ch2_guessing_game

---
+++ ch2_guessing_game/src/main.rs
@@ -0,0 +1,38 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+
+use rand::prelude::*;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is {}", secret_number);
+    
+    println!("Guess the numnber!");
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("You did not enter a number.");
+                continue;
+            },
+        };
+
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too large!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
+}
